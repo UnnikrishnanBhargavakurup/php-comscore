f9491a07e8c9977ea13c4c3c2283ad30914194dc
Unnikrishnan Bhargavakurup <unnikrishnanadoor@gmail.com>
2015-07-21 15:01:49 +0300

    Created ComscoreAPI class for making different comScore SOAP requests.

---
+++ src/Comscore/MyMetrix/ComscoreAPI.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Comscore\MyMetrix;
+
+use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
+
+
+/**
+ * comScore Web API class
+ *
+ * @package Comscore\MyMetrix
+ * @author  Unnikrishnan Bhargavakurup
+ */
+Class ComscoreAPI {
+
+  public function __construct() {
+  }
+}

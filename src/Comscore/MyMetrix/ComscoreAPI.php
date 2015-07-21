100ff7b6625e21ba681268a336f3f3f3e9588373
Unnikrishnan Bhargavakurup <unnikrishnanadoor@gmail.com>
2015-07-21 15:03:20 +0300

    Added instance function for creating singleton instances of comscoreAPI class.

--- src/Comscore/MyMetrix/ComscoreAPI.php
+++ src/Comscore/MyMetrix/ComscoreAPI.php
@@ -12,6 +12,33 @@ use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
  * @author  Unnikrishnan Bhargavakurup
  */
 Class ComscoreAPI {
+  
+  private $soap_response = null;
+  
+  /**
+   * For getting a singleton instance of comScore.
+   *
+   * @param $user_name
+   *  comscore username
+   * @param $password
+   *  comscore password
+   */
+  public static function instance($user_name, $password) {
+
+    static $instance = null;
+
+    if($instance === null) {
+      $instance = new ComscoreAPI($user_name, $password);
+      SoapWrapper::add(function ($service) {
+          $service
+              ->name('comscore')
+              ->wsdl('https://api.comscore.com/ReachFrequency/CampaignRf.asmx?WSDL')
+              ->trace(true)  
+              ->options(['login' => $user_name, 'password' => $password]);                                                // Optional: (parameter: true/false)
+      });
+    }
+    return $instance;
+  }
 
   public function __construct() {
   }

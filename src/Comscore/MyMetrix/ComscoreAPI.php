5bd5d356fe275106f82c3ac3fc1f844bd90d6afc
Unnikrishnan Bhargavakurup <unnikrishnanadoor@gmail.com>
2015-07-23 16:10:35 +0300

    added API for DiscoverParameterValues SOAP functionality for finding different parameter values.

--- src/Comscore/MyMetrix/ComscoreAPI.php
+++ src/Comscore/MyMetrix/ComscoreAPI.php
@@ -2,7 +2,7 @@
 
 namespace Comscore\MyMetrix;
 
-use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
+use SoapClient;
 
 
 /**
@@ -13,7 +13,7 @@ use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
  */
 Class ComscoreAPI {
   
-  private $soap_response = null;
+  private $soap_client;
   
   /**
    * For getting a singleton instance of comScore.
@@ -28,18 +28,43 @@ Class ComscoreAPI {
     static $instance = null;
 
     if($instance === null) {
-      $instance = new ComscoreAPI($user_name, $password);
-      SoapWrapper::add(function ($service) {
-          $service
-              ->name('comscore')
-              ->wsdl('https://api.comscore.com/ReachFrequency/CampaignRf.asmx?WSDL')
-              ->trace(true)  
-              ->options(['login' => $user_name, 'password' => $password]);                                                // Optional: (parameter: true/false)
-      });
+      $instance = new ComscoreAPI();
+      $this->soap_client = $client = new SoapClient('https://api.comscore.com/ReachFrequency/CampaignRf.asmx?WSDL', array(
+        'login' => $user_name,
+        'password' => $password,
+      ));
     }
     return $instance;
+  } 
+
+  // should not allow 'new' for creating instances
+  public function __construct() {
   }
 
-  public function __construct() {
+  /**
+   * For finding values for different parameters in SOAP request.
+   * 
+   * @param $parameter_id
+   *  Parameter for which values need to be identified
+   * @param $params
+   *  dependent parameters
+   */
+  public function discover_parameter_values($parameter_id, $params = []) {
+    $data = [
+      'parameterId' => $parameter_id,
+    ];
+    if(!empty($params)) {
+      $data['query'] = array(
+        'Parameter' => array(),
+      );
+      foreach ($params as $key => $value) {
+        $data['query']['Parameter'][] = array('KeyId' => $key,  'Value' => $value);
+      }
+    }
+    $result = $this->soap_client->__soapCall("DiscoverParameterValues", array($data));
+    if(!empty($result)) {
+      return $result->DiscoverParameterValuesResult->EnumValue;
+    }
+    return null;
   }
 }
